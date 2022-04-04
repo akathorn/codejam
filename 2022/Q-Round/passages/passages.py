b2801e3957e9e290df14f43d2d0fc8dcb35427cf
Daniel Espino <akathorn@gmail.com>
2022-04-04 16:36:46 +0200

    More work in passages.py, still doesn't pass

--- 2022/Q-Round/passages/passages.py
+++ 2022/Q-Round/passages/passages.py
@@ -13,14 +13,20 @@ err = lambda s: (sys.stderr.write(str(s) + "\n"), sys.stderr.flush())
 class Interactive:
     def __init__(self, N: int, K: int) -> None:
         self.N, self.K = N, K
+        self.visited = set()
         self.update_room()
 
-    def update_room(self):
+    def update_room(self) -> bool:
         self.room, self.passages = readmany(int)
+        if self.room in self.visited:
+            return False
+        else:
+            self.visited.add(self.room)
+            return True
 
-    def walk(self):
+    def walk(self) -> bool:
         Output("W")
-        self.update_room()
+        return self.update_room()
 
     def teleport(self, room: int):
         Output(f"T {room}")
@@ -29,15 +35,40 @@ class Interactive:
     def guess(self, E: int):
         Output(f"E {E}")
 
-    def solve(self):
-        passages = self.passages
-        rooms = list(range(1, self.N + 1))
-        random.shuffle(rooms)
-        for room in rooms[: self.K]:
-            self.teleport(room)
-            passages += self.passages
+    # # Jump when reaching a visited room
+    # def solve(self):
+    #     passages = 0
+    #     K = self.K
+    #     while K:
+    #         K -= 1
+    #         passages += self.passages
+    #         if not self.walk() and K:
+    #             room = random.randint(1, self.N)
+    #             while room in self.visited:
+    #                 room = random.randint(1, self.N)
+    #             self.teleport(room)
+    #             K -= 1
+    #     passages += self.passages
 
-        average_passages = passages / self.K
+    #     average_passages = passages / len(self.visited)
+    #     self.guess(int((average_passages * self.N) / 2))
+
+    # Don't always jump when reaching a visisted room
+    def solve(self):
+        passages = 0
+        K = self.K
+        while K:
+            K -= 1
+            passages += self.passages
+            if not self.walk() and random.random() < 0.9 and K:
+                room = random.randint(1, self.N)
+                while room in self.visited:
+                    room = random.randint(1, self.N)
+                self.teleport(room)
+                K -= 1
+        passages += self.passages
+
+        average_passages = passages / len(self.visited)
         self.guess(int((average_passages * self.N) / 2))
 
 
